651d94b807397dceb395b0110c49cdf49ef6800e
Narae Gwon <rnjs021031@gmail.com>
2023-11-15 13:14:32 +0900

    ch08 tag

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Post, Category
-from django.views.generic import ListView, DetailView
+from .models import Post, Category, Tag
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
 
 class PostList(ListView):
@@ -42,6 +47,18 @@ def category_page(request, slug):
             'categories' : Category.objects.all(),
             'no_categories_post_count' : Post.objects.filter(category=None).count(),
             'category' : category,
+        }
+    )
+
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    return render(request, 'blog/post_list.html', {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count()
         }
     )
 
